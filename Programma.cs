b371ff3e90685735ce77d18b85c894a59199b3a3
Norico Groeneveld <norico.g@live.com>
2014-11-21 21:23:30 +0100

    Form gemaakt + gitignore bijgewerkt

--- Programma.cs
+++ Programma.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace CyberPesten
 {
     class Programma
     {
-
+        static void Main()
+        {
+            Application.Run(new Venster());
+        }
     }
 }
