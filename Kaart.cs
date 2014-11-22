c87d05bec4ab61a443198902a7bb117cc258c1fc
Guido "Harb" <Harbingerofme@gmail.com>
2014-11-22 11:24:38 +0100

    Kaart klasse
    Kaart klasse geschreven met de methodes/variabelen:
    Kaart(kleur,waarde)
    kleur (waarde tussen 0-3 met de kleur, alfabetische volgorde)
    waarde (waarde tussen 1-13, waarbij 1 een aas, 11 een boer, 12 een
    vrouw, 13 een heer, en de rest normaal.)
    tekst (geeft de uitgeschreven versie van de kaart terug)

--- Kaart.cs
+++ Kaart.cs
@@ -6,7 +6,81 @@ using System.Threading.Tasks;
 
 namespace CyberPesten
 {
-    class Kaart
+    public class Kaart
     {
+        private int _kleur; //integere getal van de kleur, 0 = harten, 1 = klaver, 2 = ruiten, 3= schoppen (alfabetische volgorde)
+        private int _waarde; //waarde van de kaart, 0 doet niet mee, 1 is een aas, 2-10 komen overeen met de nummers zelf, 11 boer, 12 vrouw, 13 heer.
+
+        public Kaart(int k, int w)//maakt een nieuwe kaart, bij een ongeldige waarde wordt er een ArgumentOutOfRangeException gegooid
+        {
+            if (k > -1 && k < 4 && w > 0 && w < 14)
+            {
+                _kleur = k;
+                _waarde = w;
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException("Kaart: argument out of range. k was: " + k + " and w was: " + w + ".");
+            }
+        }
+
+        public int kleur //maak of verkrijgt de kleur van een kaart, bij een ongeldige waarde wordt er een ArgumentOutOfRangeException gegooid
+        {
+            get { return _kleur; }
+            set
+            {
+                if (value > -1 && value < 4)
+                {
+                    _kleur = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException("Kleur niet binnen accepteerbare normen.");
+                }
+            }
+        }
+
+        public int waarde
+        {
+            get { return _waarde; }
+            set
+            {
+                if (value > 0 && value < 14)
+                {
+                    _waarde = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException("Waarde niet binnen accepteerbare normen.");
+                }
+            }
+        }
+
+        public string tekst//geeft een geschreven versie van een kaart (niet dat we die nodig hebben, maar het is fijn om te hebben)
+        {
+            get
+            {
+                string a="";
+                switch (_kleur)
+                {
+                    case 0: a = "Harten ";break;
+                    case 1: a = "Klaver ";break;
+                    case 2: a = "Ruiten ";break;
+                    case 3: a = "Schoppen ";break;
+                }
+                switch (_waarde)
+                {
+                    case 1: a += "aas"; break;
+                    case 11: a += "boer"; break;
+                    case 13: a += "vrouw"; break;
+                    case 14: a += "heer"; break;
+                    default: a += ""+waarde;break;
+                }
+                return a;
+            }
+        }
+
+
+
     }
 }
--- Spel.cs
+++ Spel.cs
@@ -10,14 +10,36 @@ namespace CyberPesten
     {
         public Spel()
         {
-            List<Speler> spelers = new List<Speler>();
+            List<Speler> spelers = new List<Speler>();//moet aangeroepen worden met de Spel() functie
+            List<Kaart> oplegstapel = new List<Kaart>();
+            List<Kaart> pot = new List<Kaart>();
+            int kaartspellen;//ook deze
             //Nog aanpassen aan hoeveelheid mensen en AI
             spelers.Add(new Mens());
             for (int i = 0; i < 5; i++)
             {
                 spelers.Add(new Guido());
             }
+            kaartspellen = 1;
+
+            for (int i = 0; i < kaartspellen; i++)
+            {
+                for (int j = 0; j <4; j++)
+                {
+                    for (int k =1; k<14;k++)
+                    {
+                    pot.Add(new Kaart(j,k));
+                    }
+                }
+            }
+            pot = schud(pot);
+
             System.Diagnostics.Debug.WriteLine("Er zijn  nu " + spelers.Count + " spelers.");
+        }
+
+        public List<Kaart> schud(List<Kaart> stapel)
+        {
+            return stapel;
         }
     }
 }
