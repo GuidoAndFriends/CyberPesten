2a59c42f9d24639da79d80b457e2038644d18cf8
Guido "Harb <Harbingerofme@gmail.com>
2015-01-28 11:46:33 +0100

    Revert "klasfjalkdf"
    This reverts commit 5934daef430989e502f7c45a514c3fca73b6f68b.

--- OnlineSpeelveld.cs
+++ OnlineSpeelveld.cs
@@ -209,7 +209,7 @@ namespace CyberPesten
         new void muisOmHoog(object sender, MouseEventArgs mea)
         {
             base.muisOmhoog(sender, mea);
-            if (spel.stapel.Contains( ka))
+            if (spel.stapel[spel.stapel.Count - 1] == ka)
             {
                 stuurActie("sk:" + kaart_index);
             }
